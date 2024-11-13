8f9b150cfa257c683444ce6273d4930befa4eef1
ValentinaMolinaL <isamoli.2026@gmail.com>
2024-11-13 08:34:26 -0500

    orden de documentos

---
+++ dia 1/Consultas_Colegio_MolinaValentina.sql
@@ -0,0 +1,35 @@
+-- Consultas
+USE universidad_T2;
+
+-- Devuelve el número total de alumnas que hay.
+SELECT * FROM alumno where sexo = 'M';    -- donde
+
+
+
+-- Calcula cuántos alumnos nacieron en 1999.
+SELECT * FROM alumno where year (fecha_nacimiento) = 1999; -- donde
+
+
+
+-- Calcula cuántos profesores hay en cada departamento. El resultado sólo debe mostrar dos columnas, una con el nombre del departamento y otra con el número de profesores que hay en ese departamento. El resultado sólo debe incluir los departamentos que tienen profesores asociados y deberá estar ordenado de mayor a menor por el número de profesores.
+select count(*)cant_profesores,departamento.nombre from profesor
+inner join departamento on profesor.id_departamento = departamento.id group by departamento.id;
+
+
+
+-- Devuelve un listado con todos los departamentos y el número de profesores que hay en cada uno de ellos. Tenga en cuenta que pueden existir departamentos que no tienen profesores asociados. Estos departamentos también tienen que aparecer en el listado.
+
+
+-- Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno. Tenga en cuenta que pueden existir grados que no tienen asignaturas asociadas. Estos grados también tienen que aparecer en el listado. El resultado deberá estar ordenado de mayor a menor por el número de asignaturas.
+
+
+-- Devuelve un listado con el nombre de todos los grados existentes en la base de datos y el número de asignaturas que tiene cada uno, de los grados que tengan más de 40 asignaturas asociadas.
+
+
+-- Devuelve un listado que muestre el nombre de los grados y la suma del número total de créditos que hay para cada tipo de asignatura. El resultado debe tener tres columnas: nombre del grado, tipo de asignatura y la suma de los créditos de todas las asignaturas que hay de ese tipo. Ordene el resultado de mayor a menor por el número total de crédidos.
+
+
+-- Devuelve un listado que muestre cuántos alumnos se han matriculado de alguna asignatura en cada uno de los cursos escolares. El resultado deberá mostrar dos columnas, una columna con el año de inicio del curso escolar y otra con el número de alumnos matriculados.
+
+
+-- Devuelve un listado con el número de asignaturas que imparte cada profesor. El listado debe tener en cuenta aquellos profesores que no imparten ninguna asignatura. El resultado mostrará cinco columnas: id, nombre, primer apellido, segundo apellido y número de asignaturas. El resultado estará ordenado de mayor a menor por el número de asignaturas.
