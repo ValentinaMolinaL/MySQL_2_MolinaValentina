9f396394d9e5f0f3ccab0a4847ae12a22e0dab97
Valentina Isabel Molina Lopera <160262402+ValentinaMolinaL@users.noreply.github.com>
2024-11-20 12:58:21 -0500

    Add files via upload

---
+++ Ventas_funciones/Funciones_Ventas_MolinaValentina.sql
@@ -0,0 +1,25 @@
+-- 5 FUNCIONE ----------------------------------------------
+
+-- 1. Obtener el total de pedidos realiazados por un clientE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- 2. calcular la comisión total ganada por un comercial.
+-- 3. obtener el cliente con mayor total en pedidos
+-- 4. contar la cantidad de pedidos realizados en un año especifico.
+-- 5. obtener el promedio de total de pedidos por cliente.
